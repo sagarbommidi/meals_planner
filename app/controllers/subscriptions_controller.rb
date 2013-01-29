26e19cfb54aefc7c197f5957c15d9b712498908f
SuprajaSuresh <supraja.suresh.s@gmail.com>
2013-01-29 16:40:50 +0530

    Script for Handling subscription requests

--- app/controllers/subscriptions_controller.rb
+++ app/controllers/subscriptions_controller.rb
@@ -5,6 +5,10 @@ class SubscriptionsController < ApplicationController
     @s_type = SubscriptionType.find(params[:subscription][:subscription_type_id])
     @subscription = @s_type.subscriptions.create(params[:subscription])
     @subscription.save
+    @s_type_next = SubscriptionType.next_subscription_type
+    @s_type_current = SubscriptionType.current_subscription_type
+    @current_subscription = current_user.current_subscription if current_user.has_currently_subscribed?
+    @next_subscription = current_user.next_subscription || Subscription.new
   end
 
   def index
--- app/views/transfers/update.js.coffee
+++ app/views/transfers/update.js.coffee
@@ -1,8 +1,6 @@
 <% if @transfer.valid? %>
 <% content = escape_javascript(render :partial => 'users/todays_transfers') %>
-<% contentnew = escape_javascript(render :partial => 'users/transfer_details') %>
 $('#today_transfer').html('<%= content %>')
-$('#transfer_container').html('<%= contentnew %>')
 <% else %>
 $('#today_transfer').prepend('<p>Failed to borrow...</p>')
 <% end %>
